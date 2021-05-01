aac26c40c3ad58e66fa9df7dcb823b7c2c83c13a
Harwex213 <harwex213@mail.ru>
2021-05-01 16:49:33 +0300

    Add Clients Page

---
+++ HarwexBank/Pages/Clients/ClientsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace HarwexBank
+{
+    public partial class ClientsView : UserControl
+    {
+        public ClientsView()
+        {
+            InitializeComponent();
+
+            DataContext = new ClientsViewModel();
+        }
+    }
+}
---
+++ HarwexBank/Pages/Clients/ClientsViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HarwexBank
+{
+    public class ClientsViewModel : ObservableObject, IPageViewModel
+    {
+        public string Name => "Клиенты";
+    }
+}
