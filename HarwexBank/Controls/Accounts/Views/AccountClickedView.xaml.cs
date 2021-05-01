1b9026e784de121524c7e5e49db4f31aa527ada8
Harwex213 <harwex213@mail.ru>
2021-05-01 21:24:15 +0300

    Make second view of Account

---
+++ HarwexBank/Controls/Accounts/Views/AccountClickedView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HarwexBank
+{
+    public partial class AccountClickedView : UserControl
+    {
+        public AccountClickedView()
+        {
+            InitializeComponent();
+        }
+    }
+}
