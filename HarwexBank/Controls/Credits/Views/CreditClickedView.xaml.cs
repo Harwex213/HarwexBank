d71a6f9c97ae9145d6de35c9f1d5dc35a6a03b5e
Harwex213 <harwex213@mail.ru>
2021-05-04 21:39:54 +0300

    Make credit control

---
+++ HarwexBank/Controls/Credits/Views/CreditClickedView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HarwexBank
+{
+    public partial class CreditClickedView : UserControl
+    {
+        public CreditClickedView()
+        {
+            InitializeComponent();
+        }
+    }
+}
