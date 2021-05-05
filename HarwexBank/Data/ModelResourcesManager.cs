b0b67c55ce080a7b8eb1c56937ec9d785b66781b
Harwex213 <harwex213@mail.ru>
2021-05-05 20:51:28 +0300

    DB context manager: получаем нужных юзеров при старте

--- HarwexBank/Data/ModelResourcesManager.cs
+++ HarwexBank/Data/ModelResourcesManager.cs
@@ -1,20 +1,35 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace HarwexBank
 {
     public class ModelResourcesManager
     {
         private static ModelResourcesManager _manager;
-        private HarwexBankContext _dbContext;
+        private HarwexBankContext _context;
 
         private ModelResourcesManager()
         {
-            _dbContext = new HarwexBankContext();
+            _context = new HarwexBankContext();
         }
 
         public static ModelResourcesManager GetInstance()
         {
             return _manager ??= new ModelResourcesManager();
+        }
+        
+        
+        
+        public UserModel GetUserByLogin(string login)
+        {
+            return _context.Users.FirstOrDefault(u => u.Login == login);
+        }
+        
+        public IEnumerable<UserModel> GetAllClients()
+        {
+            return _context.Users.Where(u => u.UserType == "client").ToList();
         }
     }
 }
