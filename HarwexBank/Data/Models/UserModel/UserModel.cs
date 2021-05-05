c77706658f63ed946e2cb38531397dc6d3013fb6
Harwex213 <harwex213@mail.ru>
2021-05-05 18:04:39 +0300

    Изменил UserModel для Entity FW

---
+++ HarwexBank/Data/Models/UserModel/UserModel.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+
+namespace HarwexBank
+{
+    public class UserModel : ObservableObject
+    {
+        public UserModel()
+        {
+            Accounts = new HashSet<AccountModel>();
+            IssuedCredits = new HashSet<IssuedCreditModel>();
+        }
+        
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _patronymic;
+        private string _address;
+        private string _passport;
+        private string _login;
+        private string _password;
+        public string UserType { get; set; }
+
+        public virtual UserTypeModel UserTypeModelNavigation { get; set; }
+        public virtual ICollection<AccountModel> Accounts { get; set; }
+        public virtual ICollection<IssuedCreditModel> IssuedCredits { get; set; }
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string Patronymic
+        {
+            get => _patronymic;
+            set
+            {
+                _patronymic = value;
+                OnPropertyChanged("Patronymic");
+            }
+        }
+
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                _address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        public string Passport
+        {
+            get => _passport;
+            set
+            {
+                _passport = value;
+                OnPropertyChanged("Passport");
+            }
+        }
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+    }
+}
---
+++ HarwexBank/Data/Models/UserModel/UserTypeModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace HarwexBank
+{
+    public class UserTypeModel : ObservableObject
+    {
+        public UserTypeModel()
+        {
+            Users = new HashSet<UserModel>();
+        }
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<UserModel> Users { get; set; }
+    }
+}
