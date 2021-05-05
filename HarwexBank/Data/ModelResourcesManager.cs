317147656bbb418213abf75f7dc5191b64c8ff49
Harwex213 <harwex213@mail.ru>
2021-05-05 18:32:02 +0300

    Определяем класс-менеджер по работе с контекстом DB

---
+++ HarwexBank/Data/ModelResourcesManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HarwexBank
+{
+    public class ModelResourcesManager
+    {
+        private static ModelResourcesManager _manager;
+        private HarwexBankContext _dbContext;
+
+        private ModelResourcesManager()
+        {
+            _dbContext = new HarwexBankContext();
+        }
+
+        public static ModelResourcesManager GetInstance()
+        {
+            return _manager ??= new ModelResourcesManager();
+        }
+    }
+}
