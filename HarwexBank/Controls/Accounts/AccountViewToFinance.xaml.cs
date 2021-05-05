7f6bc1f5507358530122e238b0bb2ab592d7023c
Harwex213 <harwex213@mail.ru>
2021-05-06 02:29:18 +0300

    Подправил вёрстку моделей Account; Сделал вёрстку Account'a для FinancePage

---
+++ HarwexBank/Controls/Accounts/AccountViewToFinance.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HarwexBank
+{
+    public partial class AccountViewToFinance : UserControl
+    {
+        public AccountViewToFinance()
+        {
+            InitializeComponent();
+        }
+    }
+}
