d1953f1f7a3fd9cf0a6b1066b9d4b9c5dd998d9d
Harwex213 <harwex213@mail.ru>
2021-05-07 00:12:52 +0300

    Попытка сделать выдвижное меню

--- HarwexBank/Controls/Accounts/AccountViewModel.cs
+++ HarwexBank/Controls/Accounts/AccountViewModel.cs
@@ -3,16 +3,73 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace HarwexBank
 {
     public class AccountViewModel : ObservableObject, IControlViewModel
     {
         public string Name => "Счета";
-        public ObservableCollection<AccountModel> AccountModels { get; }
         public AccountViewModel()
         {
             AccountModels = new ObservableCollection<AccountModel>(ApplicationViewModel.LoggedInUser.Accounts);
         }
+        public ObservableCollection<AccountModel> AccountModels { get; }
+
+        #region Commands
+
+        #region Files & Properties
+
+        private ICommand _openAccountCommand;
+        private ICommand _closeAccountCommand;
+        private ICommand _freezeAccountCommand;
+
+        // public ICommand OpenAccountCommand
+        // {
+        //     get
+        //     {
+        //         _openAccountCommand ??= new RelayCommand(
+        //             p => ChangeViewModel((IPageViewModel)p),
+        //             p => p is IPageViewModel);
+        //
+        //         return _openAccountCommand;
+        //     }
+        // }
+        //
+        // public ICommand CloseAccountCommand
+        // {
+        //     get
+        //     {
+        //         _closeAccountCommand ??= new RelayCommand(
+        //             p => ChangeViewModel((IPageViewModel)p),
+        //             p => p is IPageViewModel);
+        //
+        //         return _closeAccountCommand;
+        //     }
+        // }
+        //
+        // public ICommand FreezeAccountCommand
+        // {
+        //     get
+        //     {
+        //         _freezeAccountCommand ??= new RelayCommand(
+        //             p => ChangeViewModel((IPageViewModel)p),
+        //             p => p is IPageViewModel);
+        //
+        //         return _freezeAccountCommand;
+        //     }
+        // }
+        
+        #endregion
+
+        #region Methods
+        
+        
+
+        #endregion
+
+        #endregion
+        
     }
 }
--- HarwexBank/Controls/Accounts/AccountViewToFinancePage.xaml.cs
+++ HarwexBank/Controls/Accounts/AccountViewToFinancePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace HarwexBank
 {
@@ -8,5 +11,33 @@ namespace HarwexBank
         {
             InitializeComponent();
         }
+        
+        // public IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        // {
+        //     if (depObj != null)
+        //     {
+        //         for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+        //         {
+        //             DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+        //
+        //             if (child != null && child is T)
+        //                 yield return (T)child;
+        //
+        //             foreach (T childOfChild in FindVisualChildren<T>(child))
+        //                 yield return childOfChild;
+        //         }
+        //     }
+        // }
+        //
+        // private void AccountGeneralInfo_OnClick(object sender, RoutedEventArgs e)
+        // {
+        //     foreach (var checkBox in FindVisualChildren<CheckBox>(AccountsList))
+        //     {
+        //         if (checkBox.Name == "TrashCheckBox")
+        //         {
+        //             checkBox.IsChecked = !checkBox.IsChecked;
+        //         }
+        //     }
+        // }
     }
 }
