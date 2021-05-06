905eee70255e0b409a9e67027f332dd250cf4d03
Harwex213 <harwex213@mail.ru>
2021-05-06 13:15:10 +0300

    Вёрстка: Переделал AccountView в Custom Controls

---
+++ HarwexBank/Controls/Accounts/AccountView.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace HarwexBank
+{
+    public class AccountGeneralInfo : Control { }
+    public class AccountAdditionalInfo : Control { }
+    public class OpenAccount : Control { }
+    public class CloseAccount : Control { }
+    public class FreezeAccount : Control { }
+}
