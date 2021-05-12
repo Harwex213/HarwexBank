b8c667d2d7af63c4db6fe2ffffcaaf78e94fb6f1
Harwex213 <harwex213@mail.ru>
2021-05-12 12:12:00 +0300

    BackEnd of JournalPage done

--- HarwexBank/Application/Pages/ForMainWindow/Journal/JournalViewModel.cs
+++ HarwexBank/Application/Pages/ForMainWindow/Journal/JournalViewModel.cs
@@ -1,7 +1,31 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HarwexBank
 {
+    public class AllJournalTab : Control, IControlViewModel
+    {
+        public new string Name => "Всё";
+    }
+    
+    public class OperationsTab : Control, IControlViewModel
+    {
+        public new string Name => "Переводы";
+    }
+
+    public class CreditRepaymentsTab : Control, IControlViewModel
+    {
+        public new string Name => "Погашение кредитов";
+    }
+    
+    public class NotificationsTab : Control, IControlViewModel
+    {
+        public new string Name => "Уведомления";
+    }
+
     public class JournalViewModel : BaseControlViewModel, IControlViewModel
     {
         public string Name => "Журнал";
@@ -14,8 +38,47 @@ namespace HarwexBank
                 "client" => MainViewModel.Data.UserJournal,
                 _ => null
             };
+
+            RefreshPage();
+            
+            ControlViewModels.Add(new AllJournalTab());
+            ControlViewModels.Add(new OperationsTab());
+            ControlViewModels.Add(new CreditRepaymentsTab());
+            ControlViewModels.Add(new NotificationsTab());
+            
+            SelectedControlViewModel = ControlViewModels[0];
         }
         
         public ObservableCollection<JournalModel> Journal { get; set; }
+        public List<JournalModel> Transfers { get; set; }
+        public List<JournalModel> CreditRepayments { get; set; }
+        public List<JournalModel> Notifications { get; set; }
+        
+        #region Commands
+
+        // Fields.
+        private ICommand _refreshPageCommand;
+        
+        // Props.
+        public ICommand RefreshPageCommand
+        {
+            get
+            {
+                _refreshPageCommand ??= new RelayCommand(
+                    _ => RefreshPage());
+        
+                return _refreshPageCommand;
+            }
+        }
+        
+        // Methods.
+        private void RefreshPage()
+        {
+            Transfers = Journal?.Where(j => j is TransferToAccountModel).ToList();
+            CreditRepayments = Journal?.Where(j => j is CreditRepaymentModel).ToList();
+            Notifications = Journal?.Where(j => j is NotificationModel).ToList();
+        }
+
+        #endregion
     }
 }
