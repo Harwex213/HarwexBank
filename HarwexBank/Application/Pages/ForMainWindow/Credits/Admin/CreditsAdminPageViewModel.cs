dc19d5629e89866c5aa4061289b7f810f7f158dd
Harwex213 <harwex213@mail.ru>
2021-05-13 21:57:34 +0300

    All minimum functionality done

--- HarwexBank/Application/Pages/ForMainWindow/Credits/Admin/CreditsAdminPageViewModel.cs
+++ HarwexBank/Application/Pages/ForMainWindow/Credits/Admin/CreditsAdminPageViewModel.cs
@@ -1,7 +1,186 @@
-﻿namespace HarwexBank
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HarwexBank
 {
+    public class CreditTypesListViewModel : Control, IControlViewModel { }
+    public class CreateNewCreditTypeViewModel : Control, IControlViewModel { }
+    public class UpdateExistedCreditTypeViewModel : Control, IControlViewModel { }
+    
     public class CreditsAdminPageViewModel : BaseControlViewModel, IControlViewModel
     {
         public string Name => "Кредиты";
+
+        public CreditsAdminPageViewModel()
+        {
+            ExistedCreditTypes = MainViewModel.Data.ExistedCreditTypes;
+            
+            ControlViewModels.Add(new CreditTypesListViewModel());
+            ControlViewModels.Add(new CreateNewCreditTypeViewModel());
+            ControlViewModels.Add(new UpdateExistedCreditTypeViewModel());
+
+            SelectedControlViewModel = ControlViewModels.FirstOrDefault();
+
+            InitializeNewData();
+        }
+
+        public ObservableCollection<CreditTypeModel> ExistedCreditTypes { get; set; }
+        public CreditTypeModel SelectedCreditType { get; set; }
+        public CreditTypeModel CreditTypeToCreate { get; set; }
+
+        private void InitializeNewData()
+        {
+            CreditTypeToCreate = new CreditTypeModel();
+        }
+
+        #region Commands
+
+        // Fields.
+        private ICommand _goToCreditTypesListCommand;
+        private ICommand _goToCreateNewCreditTypeCommand;
+        private ICommand _goToUpdateExistedCreditTypeCommand;
+        
+        private ICommand _selectCreditTypeCommand;
+        private ICommand _saveUpdatesOfCreditTypeCommand;
+        private ICommand _deleteCreditTypeCommand;
+        
+        private ICommand _createNewCreditTypeCommand;
+        
+        // Props.
+        public ICommand GoToCreditTypesListCommand
+        {
+            get
+            {
+                _goToCreditTypesListCommand ??= new RelayCommand(
+                    _ => GoToCreditTypesList());
+        
+                return _goToCreditTypesListCommand;
+            }
+        }
+
+        public ICommand GoToCreateNewCreditTypeCommand
+        {
+            get
+            {
+                _goToCreateNewCreditTypeCommand ??= new RelayCommand(
+                    _ => GoToCreateNewCreditType());
+        
+                return _goToCreateNewCreditTypeCommand;
+            }
+        }
+
+        public ICommand GoToUpdateExistedCreditTypeCommand
+        {
+            get
+            {
+                _goToUpdateExistedCreditTypeCommand ??= new RelayCommand(
+                    _ => GoToUpdateExistedCreditType());
+        
+                return _goToUpdateExistedCreditTypeCommand;
+            }
+        }
+        
+        public ICommand SelectCreditTypeCommand
+        {
+            get
+            {
+                _selectCreditTypeCommand ??= new RelayCommand(
+                    c => SelectCreditType((CreditTypeModel) c),
+                    c => c is CreditTypeModel);
+
+                return _selectCreditTypeCommand;
+            }
+        }
+        
+        public ICommand SaveUpdatesOfCreditTypeCommand
+        {
+            get
+            {
+                _saveUpdatesOfCreditTypeCommand ??= new RelayCommand(
+                    _ => SaveUpdatesOfCreditType());
+
+                return _saveUpdatesOfCreditTypeCommand;
+            }
+        }
+        
+        public ICommand DeleteCreditTypeCommand
+        {
+            get
+            {
+                _deleteCreditTypeCommand ??= new RelayCommand(
+                    _ => DeleteCreditType());
+
+                return _deleteCreditTypeCommand;
+            }
+        }
+
+        public ICommand CreateNewCreditTypeCommand
+        {
+            get
+            {
+                _createNewCreditTypeCommand ??= new RelayCommand(
+                    _ => CreateNewCreditType());
+
+                return _createNewCreditTypeCommand;
+            }
+        }
+
+        // Methods.
+        private void GoToCreditTypesList()
+        {
+            SelectedControlViewModel = ControlViewModels[0];
+        }
+
+        private void GoToCreateNewCreditType()
+        {
+            SelectedControlViewModel = ControlViewModels[1];
+        }
+
+        private void GoToUpdateExistedCreditType()
+        {
+            SelectedControlViewModel = ControlViewModels[2];
+        }
+
+        private void SelectCreditType(CreditTypeModel creditTypeModel)
+        {
+            SelectedCreditType = creditTypeModel;
+            GoToUpdateExistedCreditType();
+        }
+
+        private void SaveUpdatesOfCreditType()
+        {
+            ModelResourcesManager.GetInstance().UpdateModel(SelectedCreditType);
+
+            MessageBox.Show("Изменения успешно применены");
+        }
+        
+        private void DeleteCreditType()
+        {
+            var boxResult = MessageBox.Show("Вы действительно хотите удалить данный вид кредита?",
+                "HarwexBank", MessageBoxButton.YesNo);
+
+            if (boxResult != MessageBoxResult.Yes) 
+                return;
+            
+            ModelResourcesManager.GetInstance().RemoveModel(SelectedCreditType);
+            ExistedCreditTypes.Remove(SelectedCreditType);
+            
+            GoToCreditTypesList();
+        }
+
+        private void CreateNewCreditType()
+        {
+            ModelResourcesManager.GetInstance().AddModel(CreditTypeToCreate);
+            ExistedCreditTypes.Add(CreditTypeToCreate);
+            InitializeNewData();
+
+            GoToCreditTypesList();
+        }
+
+        #endregion
     }
 }
--- HarwexBank/Data/Models/CreditModel/CreditTypeModel.cs
+++ HarwexBank/Data/Models/CreditModel/CreditTypeModel.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace HarwexBank
 {
-    public class CreditTypeModel : ObservableObject, ModelResourcesManager.IModel
+    public class CreditTypeModel : ObservableObject, ModelResourcesManager.IModel, IDataErrorInfo
     {
         public CreditTypeModel()
         {
@@ -43,5 +45,39 @@ namespace HarwexBank
                 OnPropertyChanged("Rate");
             }
         }
+
+        #region Data Validation
+
+        public string Error => null;
+
+        public string this[string name]
+        {
+            get
+            {
+                switch (name)
+                {
+                    case nameof(Name):
+                        if (string.IsNullOrEmpty(Name) || Name.Length is < 6 or > 50)
+                        {
+                            return "Должно быть длиннее 6 символов";
+                        }
+                        else if (!Regex.Match(Name ?? string.Empty, @"(^[A-Z][a-zA-Z]*$)|(^[А-я][а-яА-я]*$)").Success)
+                        {
+                            return "Должно содержать только буквы";
+                        }
+                        break;
+                    case nameof(Rate):
+                        if (Rate is < 0 or > 1)
+                        {
+                            return "Процентная ставка должна быть не больше 1 и не меньше 0";
+                        }
+                        break;
+                }
+
+                return null;
+            }
+        }
+
+        #endregion
     }
 }
