cdf6b448cc3e04918ad5073e386a0ab9a70bcf6d
Harwex213 <harwex213@mail.ru>
2021-05-18 10:52:48 +0300

    Style for MainView

--- HarwexBank/Application/Windows/MainWindow/MainView.xaml.cs
+++ HarwexBank/Application/Windows/MainWindow/MainView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace HarwexBank
 {
@@ -7,6 +9,28 @@ namespace HarwexBank
         public MainView()
         {
             InitializeComponent();
+        }
+        private int _barElementsCount = 0;
+        
+        private void LeftBarToggleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            for(var i = 0; i < LeftNavbar.Items.Count; i++)
+            {
+                var uiElement = (ContentPresenter)LeftNavbar.ItemContainerGenerator.ContainerFromIndex(i);
+                var item = (uiElement.ContentTemplate.FindName("LeftBarToggleButton", uiElement) as ToggleButton);
+                item.IsChecked = false;
+            }
+        
+            ((ToggleButton) sender).IsChecked = true;
+        }
+        
+        private void LeftBarToggleButton_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _barElementsCount++;
+            if (_barElementsCount < 2)
+            {
+                LeftBarToggleButton_OnClick(sender, e);
+            }
         }
     }
 }
