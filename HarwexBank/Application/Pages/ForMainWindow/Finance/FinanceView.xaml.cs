853877c78bbb0cff6cf5eff0ba85862dd8668e2d
Harwex213 <harwex213@mail.ru>
2021-05-18 11:16:04 +0300

    Finance Page style

--- HarwexBank/Application/Pages/ForMainWindow/Finance/FinanceView.xaml.cs
+++ HarwexBank/Application/Pages/ForMainWindow/Finance/FinanceView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 
 namespace HarwexBank
@@ -9,6 +10,28 @@ namespace HarwexBank
         public FinanceView()
         {
             InitializeComponent();
+        }
+        private int _barElementsCount;
+        
+        private void FinanceBarToggleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            for(var i = 0; i < FinanceBar.Items.Count; i++)
+            {
+                var uiElement = (ContentPresenter)FinanceBar.ItemContainerGenerator.ContainerFromIndex(i);
+                var item = (uiElement.ContentTemplate.FindName("FinanceBarToggleButton", uiElement) as ToggleButton);
+                item.IsChecked = false;
+            }
+        
+            ((ToggleButton) sender).IsChecked = true;
+        }
+        
+        private void FinanceBarToggleButton_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _barElementsCount++;
+            if (_barElementsCount < 2)
+            {
+                FinanceBarToggleButton_OnClick(sender, e);
+            }
         }
     }
 }
