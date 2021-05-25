8f789cf528059cc7545d429e301e5a69114de36a
Harwex213 <harwex213@mail.ru>
2021-05-25 19:46:11 +0300

    Список счётов готов

---
+++ HarwexBank/Application/Controls/Accounts/AccountControlsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HarwexBank
+{
+    public partial class AccountControlsView
+    {
+        void AccountAdditionalInfo_Loaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            ((Border) sender).Visibility = Visibility.Collapsed;
+        }
+    }
+}
