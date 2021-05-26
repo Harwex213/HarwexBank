b3d36e4e15bfc81c559e616320e117ce6cb2c38e
Harwex213 <harwex213@mail.ru>
2021-05-26 20:59:19 +0300

    Костыль для динамического переноса блока в InfoBlock

--- HarwexBank/Application/Controls/Credits/CreditControlsView.xaml.cs
+++ HarwexBank/Application/Controls/Credits/CreditControlsView.xaml.cs
@@ -9,5 +9,47 @@ namespace HarwexBank
         {
             ControlsBase.CollapseVisibility_OnLoad(sender, routedEventArgs);
         }
+
+        private void FrameworkElement_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (((Grid)sender).ActualWidth < 700)
+            {
+                if (((Grid) sender).ColumnDefinitions.Count == 5)
+                    return;
+                
+                Grid.SetRow(((Grid)sender).Children[2], 1);
+                Grid.SetRow(((Grid)sender).Children[3], 1);
+                Grid.SetColumn(((Grid)sender).Children[2], 1);
+                Grid.SetColumn(((Grid)sender).Children[3], 3);
+                
+                ((Grid) sender).ColumnDefinitions.RemoveAt(8);
+                ((Grid) sender).ColumnDefinitions.RemoveAt(7);
+                ((Grid) sender).ColumnDefinitions.RemoveAt(6);
+                ((Grid) sender).ColumnDefinitions.RemoveAt(5);
+            }
+            else
+            {
+                if (((Grid) sender).ColumnDefinitions.Count == 9)
+                    return;
+                
+                ((Grid) sender).ColumnDefinitions.Add(new ColumnDefinition
+                    // ReSharper disable once PossibleNullReferenceException
+                    {Width = (GridLength) new GridLengthConverter().ConvertFrom("Auto")});
+                ((Grid) sender).ColumnDefinitions.Add(new ColumnDefinition
+                    // ReSharper disable once PossibleNullReferenceException
+                    {Width = (GridLength) new GridLengthConverter().ConvertFrom("*")});
+                ((Grid) sender).ColumnDefinitions.Add(new ColumnDefinition
+                    // ReSharper disable once PossibleNullReferenceException
+                    {Width = (GridLength) new GridLengthConverter().ConvertFrom("Auto")});
+                ((Grid) sender).ColumnDefinitions.Add(new ColumnDefinition
+                    // ReSharper disable once PossibleNullReferenceException
+                    {Width = (GridLength) new GridLengthConverter().ConvertFrom("*")});
+                
+                Grid.SetRow(((Grid)sender).Children[2], 0);
+                Grid.SetRow(((Grid)sender).Children[3], 0);
+                Grid.SetColumn(((Grid)sender).Children[2], 5);
+                Grid.SetColumn(((Grid)sender).Children[3], 7);
+            }
+        }
     }
 }
--- HarwexBank/Application/Controls/Credits/InnerPagesViewModels/TakeNewCreditViewModel.cs
+++ HarwexBank/Application/Controls/Credits/InnerPagesViewModels/TakeNewCreditViewModel.cs
@@ -2,6 +2,6 @@
 {
     public class TakeNewCreditViewModel : IControlViewModel
     {
-        public string Name => "";
+        public string Name => "Взятие нового кредита";
     }
 }
