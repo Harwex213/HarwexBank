a31367d556d37c5e3a7830e332e25c660ce0a331
Harwex213 <harwex213@mail.ru>
2021-05-27 21:19:15 +0300

    Clients list & info migration done

--- HarwexBank/Application/Pages/ForMainWindow/Clients/ClientsView.xaml.cs
+++ HarwexBank/Application/Pages/ForMainWindow/Clients/ClientsView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
 namespace HarwexBank
 {
@@ -7,6 +11,56 @@ namespace HarwexBank
         public ClientsView()
         {
             InitializeComponent();
+        }
+        private int _barElementsCount;
+        
+        public IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+
+                    if (child != null && child is T)
+                        yield return (T)child;
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                        yield return childOfChild;
+                }
+            }
+        }
+        
+        private void ControlsBarToggleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var itemsControl in FindVisualChildren<ItemsControl>(this))
+            {
+                if (itemsControl.Name != "ClientsInfoBar")
+                    continue;
+                
+                for(var i = 0; i < itemsControl.Items.Count; i++)
+                {
+                    var uiElement = (ContentPresenter)itemsControl.ItemContainerGenerator.ContainerFromIndex(i);
+                    var item = (uiElement.ContentTemplate.FindName("ControlsBarToggleButton", uiElement) as ToggleButton);
+                    item.IsChecked = false;
+                }
+        
+                ((ToggleButton) sender).IsChecked = true;
+            }
+        }
+        
+        private void ControlsBarToggleButton_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _barElementsCount++;
+            if (_barElementsCount < 2)
+            {
+                ControlsBarToggleButton_OnClick(sender, e);
+            }
+        }
+
+        private void ControlsBar_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _barElementsCount = 0;
         }
     }
 }
