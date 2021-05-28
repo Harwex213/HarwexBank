d241b142b9c53ab112eaa2f6a8fbc7c691c19fe0
Harwex213 <harwex213@mail.ru>
2021-05-29 02:13:16 +0300

    feat(credit): добавил расчёт аннуитетного платежа

---
+++ HarwexBank/Data/CreditCalculator/CreditCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HarwexBank.CreditCalculator
+{
+    public class CreditCalculator
+    {
+        public CreditCalculator(decimal creditAmount, decimal creditRate, int creditTerm)
+        {
+            _creditAmount = creditAmount;
+            _creditRate = creditRate / 100 / 12;
+            _creditTerm = creditTerm;
+        }
+
+        private readonly decimal _creditAmount;
+        private readonly decimal _creditRate;
+        private readonly int _creditTerm;
+
+        public decimal GetAnnuityPlan()
+        {
+            return _creditAmount * (decimal) Math.Pow((double) (1 + _creditRate), _creditTerm) /
+                   (decimal) (Math.Pow((double) (1 + _creditRate), _creditTerm) - 1);
+        }
+    }
+}
